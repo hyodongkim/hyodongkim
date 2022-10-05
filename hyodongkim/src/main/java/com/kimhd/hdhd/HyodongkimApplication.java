c33d039f4a34b58b059649ca71b5c7c96cd2655e
hyodongkim <kimhd853@naver.com>
2022-10-06 07:54:02 +0900

    TETETEST

---
+++ hyodongkim/src/main/java/com/kimhd/hdhd/HyodongkimApplication.java
@@ -0,0 +1,13 @@
+package com.kimhd.hdhd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyodongkimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyodongkimApplication.class, args);
+	}
+
+}
